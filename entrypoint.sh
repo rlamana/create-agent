9034659e805ecc01526cddb70fb67b9e36e9789e
Ramon Lamana <rlamana@gmail.com>
2025-06-04 16:05:33 +0200

    Test removing checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,15 @@ repository=$2
 
 echo "Creating an agent..."
 
-if [ -z "$prompt" ]; then
-  echo "A prompt is required"
-  exit 1
-fi
+# if [ -z "$prompt" ]; then
+#   echo "A prompt is required"
+#   exit 1
+# fi
 
-if [ -z "$repository" ]; then
-  echo "A repository is required"
-  exit 1
-fi
-
-if [ -z "$OKTETO_TOKEN" ]; then
-  echo "The Okteto token is required"
-  exit 1
-fi
-
-if [ -z "$OKTETO_CONTEXT" ]; then
-  echo "The Okteto context is required"
-  exit 1
-fi
+# if [ -z "$repository" ]; then
+#   echo "A repository is required"
+#   exit 1
+# fi
 
 echo "Working on the Okteto context: $OKTETO_CONTEXT"
 
