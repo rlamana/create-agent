e6eb5d08456dcf94f5ed2abee0c06ea34ecb281b
Ramon Lamana <rlamana@gmail.com>
2025-06-04 15:54:26 +0200

    Show what is going on with envars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,27 @@ set -e
 prompt=$1
 repository=$2
 
-if [ -z $prompt ]; then
+if [ -z "$prompt" ]; then
   echo "A prompt is required"
   exit 1
 fi
 
-if [ -z $repository ]; then
+if [ -z "$repository" ]; then
   echo "A repository is required"
   exit 1
 fi
+
+if [ -z "$OKTETO_TOKEN" ]; then
+  echo "The Okteto token is required"
+  exit 1
+fi
+
+if [ -z "$OKTETO_CONTEXT" ]; then
+  echo "The Okteto context is required"
+  exit 1
+fi
+
+echo "Working on the Okteto context: $OKTETO_CONTEXT"
 
 /create-agent.js $prompt $repository $OKTETO_TOKEN $OKTETO_CONTEXT
 if [ $? -ne 0 ]; then
